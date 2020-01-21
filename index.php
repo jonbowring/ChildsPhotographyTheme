4d66a6e60785f71f7fd33396b5971a2368e02b1b
Jonathon Bowring <jbowring@informatica.com>
2020-01-21 22:28:03 +1000

    Skeleton of the index.php page created

--- index.php
+++ index.php
@@ -1,3 +1,36 @@
-<?php get_header(); ?>
+<?php 
+/**
+ * The main template file
+ *
+ * This is the most generic template file in a WordPress theme
+ * and one of the two required files for a theme (the other being style.css).
+ * It is used to display a page when nothing more specific matches a query.
+ * E.g., it puts together the home page when no home.php file exists.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package WordPress
+ * @subpackage Twenty_Nineteen
+ * @since 1.0.0
+ */
+
+define( 'WP_USE_THEMES', false );
+get_header(); 
+?>
+
+<?php 
+if ( have_posts() ) {
+	while ( have_posts() ) {
+		the_post(); 
+		//
+		// Post Content here
+		//
+	} // end while
+} // end if
+?>
+
+<?php get_sidebar(); ?>
+
+<?php get_search_form(); ?>
 
 <?php get_footer(); ?>
